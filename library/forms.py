bd3daf4afb6b5ff2d71f1d9290a9303ed8d39a87
Abhishek Dixit <83164668+Abhishek-op@users.noreply.github.com>
2022-09-28 20:24:10 -0500

    Add files via upload

---
+++ library/forms.py
@@ -0,0 +1,54 @@
+from django import forms
+from django.contrib.auth.models import User
+from . import models
+
+class IssueBookForm(forms.Form):
+    isbn2 = forms.ModelChoiceField(queryset=models.Book.objects.all(), empty_label="Book Name [ISBN]", to_field_name="isbn", label="Book (Name and ISBN)")
+    name2 = forms.ModelChoiceField(queryset=models.Student.objects.all(), empty_label="Name [Branch] [Class] [Roll No]", to_field_name="user", label="Student Details")
+    
+    isbn2.widget.attrs.update({'class': 'form-control'})
+    name2.widget.attrs.update({'class':'form-control'})
+
+
+
+from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
+from django.forms.fields import EmailField
+from django.forms.forms import Form
+
+
+class CustomUserCreationForm(UserCreationForm):
+    username = forms.CharField(label='username', min_length=5, max_length=150)
+    email = forms.EmailField(label='email')
+    password1 = forms.CharField(label='password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm password', widget=forms.PasswordInput)
+
+    def username_clean(self):
+        username = self.cleaned_data['username'].lower()
+        new = User.objects.filter(username=username)
+        if new.count():
+            raise ValidationError("User Already Exist")
+        return username
+
+    def email_clean(self):
+        email = self.cleaned_data['email'].lower()
+        new = User.objects.filter(email=email)
+        if new.count():
+            raise ValidationError(" Email Already Exist")
+        return email
+
+    def clean_password2(self):
+        password1 = self.cleaned_data['password1']
+        password2 = self.cleaned_data['password2']
+
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Password don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = User.objects.create_user(
+            self.cleaned_data['username'],
+            self.cleaned_data['email'],
+            self.cleaned_data['password1']
+        )
+        return user
---
+++ library/urls.py
@@ -0,0 +1,29 @@
+from django.urls import path
+from . import views
+
+from django.contrib import admin
+from library.views import register
+
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("add_book/", views.add_book, name="add_book"),
+    path("view_books/", views.view_books, name="view_books"),
+    path("view_students/", views.view_students, name="view_students"),
+    path("issue_book/", views.issue_book, name="issue_book"),
+    path("view_issued_book/", views.view_issued_book, name="view_issued_book"),
+    path("student_issued_books/", views.student_issued_books, name="student_issued_books"),
+    path("profile/", views.profile, name="profile"),
+    path("edit_profile/", views.edit_profile, name="edit_profile"),
+    path('admin/', admin.site.urls),
+    path('register', register, name='register'),
+    path("admin_registration/", views.admin_registration, name="admin_registration"),
+    path("change_password/", views.change_password, name="change_password"),
+    path("student_login/", views.student_login, name="student_login"),
+    path("admin_login/", views.admin_login, name="admin_login"),
+    path("logout/", views.Logout, name="logout"),
+
+    path("delete_book/<int:myid>/", views.delete_book, name="delete_book"),
+    path("delete_student/<int:myid>/", views.delete_student, name="delete_student"),
+]
+
